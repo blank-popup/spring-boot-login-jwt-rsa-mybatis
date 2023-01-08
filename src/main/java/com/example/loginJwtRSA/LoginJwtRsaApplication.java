87ea9886ddb74e4433d5e3c3d3dd970bfd11f5d4
blank_popup <ha6446@gmail.com>
2023-01-08 22:49:22 +0900

    00 start project

---
+++ src/main/java/com/example/loginJwtRSA/LoginJwtRsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginJwtRSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginJwtRsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginJwtRsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loginJwtRSA/LoginJwtRsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginJwtRSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginJwtRsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
