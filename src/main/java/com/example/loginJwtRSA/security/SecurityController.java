bb95a73e59a75fdaff3a6dc618ebabad05f56fa8
blank_popup <ha6446@gmail.com>
2023-01-08 23:18:56 +0900

    02 use application.yml

--- src/main/java/com/example/loginJwtRSA/security/SecurityController.java
+++ src/main/java/com/example/loginJwtRSA/security/SecurityController.java
@@ -1,6 +1,7 @@
 package com.example.loginJwtRSA.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,19 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/security")
 public class SecurityController {
+    @Value("${str.common}")
+    private String strCommon;
+    @Value("${str.individual}")
+    private String strIndivudual;
     @PostMapping("/signin")
     public ResponseEntity<?> signin(@RequestBody ModelUser modelUser) {
-        log.trace("$$$$$ RestController trace login start");
-        log.debug("$$$$$ RestController debug login start");
-        log.info("$$$$$ RestController info login start");
-        log.warn("$$$$$ RestController warn login start");
-        log.error("$$$$$ RestController error login start");
+        log.trace("$$$$$ RestController trace login start {} {}", strCommon, strIndivudual);
+        log.debug("$$$$$ RestController debug login start {} {}", strCommon, strIndivudual);
+        log.info("$$$$$ RestController info login start {} {}", strCommon, strIndivudual);
+        log.warn("$$$$$ RestController warn login start {} {}", strCommon, strIndivudual);
+        log.error("$$$$$ RestController error login start {} {}", strCommon, strIndivudual);
         if ("18".equals(modelUser.getPassword())) {
             log.info("$$$$$ RestController login success");
             modelUser.setUsername("ImPossible");
         }
         else {
-            log.info("$$$$$ RestController login fialure");
+            log.warn("$$$$$ RestController login fialure");
             modelUser.setUsername("ImnotUser");
         }
         return ResponseEntity.status(HttpStatus.OK).body(modelUser);
