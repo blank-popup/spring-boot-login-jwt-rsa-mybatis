093f171ffbd9722ff6b93781d105436bca709781
blank_popup <ha6446@gmail.com>
2023-08-18 07:29:16 +0900

    Alternative to aspect pointcut option

--- src/main/java/com/example/loginJwtRSA/aop/AspectLog.java
+++ src/main/java/com/example/loginJwtRSA/aop/AspectLog.java
@@ -9,29 +9,30 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Slf4j
 public class AspectLog {
+//    @Pointcut("execution(* com.example.loginJwtRSA..Controller*.*(..))")
     @Pointcut("within(com.example.loginJwtRSA..*)")
     public void controller() {}
 
     @Before("controller()")
     public void beforeRequest(JoinPoint joinPoint) {
-        log.info("##### Start request {}", joinPoint.getSignature().toShortString());
-        log.info("Parameter request {}", joinPoint.getArgs());
+        log.info("##### Start request : {}", joinPoint.getSignature().toShortString());
+        log.info("Parameter request : {}", joinPoint.getArgs());
     }
 
     @AfterReturning(pointcut = "controller()", returning = "returnValue")
     public void afterReturningLogging(JoinPoint joinPoint, Object returnValue) {
-        log.info("##### End request {}", joinPoint.getSignature().toShortString());
         if (returnValue == null) {
             log.info("returnValue : NULL");
         }
         else {
             log.info("returnValue : {}", returnValue.toString());
         }
+        log.info("##### End request : {}", joinPoint.getSignature().toShortString());
     }
 
     @AfterThrowing(pointcut = "controller()", throwing = "e")
     public void afterThrowingLogging(JoinPoint joinPoint, Exception e) {
-        log.error("##### Occured error in request {}", joinPoint.getSignature().toShortString());
+        log.error("##### Occured error in request : {}", joinPoint.getSignature().toShortString());
         log.error("Message : {}", e.getMessage());
     }
 }
