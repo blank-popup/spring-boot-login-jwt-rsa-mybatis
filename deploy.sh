3b251c415d3eb5506ad7caba970d58c183af7a47
blank_popup <ha6446@gmail.com>
2023-12-23 02:51:41 +0900

    Test Jenkins WorkSpace

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@ WORKSPACE=$1
 NAME_PROJECT="template"
 DIRECTORY="/home/JENKINS/template/api/"
 FILENAME="loginJwtRSA-0.0.1-SNAPSHOT.jar"
-PROFILE="develop"
+VALUE_PROFILE="develop"
 FILEPATH=${DIRECTORY}${FILENAME}
-OPTION="-Dspring.profiles.active=${PROFILE}"
+KEY_PROFILE="spring.profiles.active"
+OPTION="-D${KEY_PROFILE}=${VALUE_PROFILE}"
 
 java -version
 
@@ -15,13 +16,14 @@ echo WORKSPACE : ${WORKSPACE}
 echo NAME_PROJECT : ${NAME_PROJECT}
 echo DIRECTORY : ${DIRECTORY}
 echo FILENAME : ${FILENAME}
+echo VALUE_PROFILE : ${VALUE_PROFILE}
 echo FILEPATH : ${FILEPATH}
-echo PROFILE : ${PROFILE}
+echo KEY_PROFILE : ${KEY_PROFILE}
 echo OPTION : ${OPTION}
 
 echo ========== Terminating ${NAME_PROJECT} process ==========
 
-PID=`ps -ef | grep ${FILEPATH} | grep ${OPTION} | grep -v grep | awk '{print $2}'`
+PID=`ps -ef | grep ${FILEPATH} | grep ${KEY_PROFILE}=${VALUE_PROFILE} | grep -v grep | awk '{print $2}'`
 echo PID : ${PID}
 
 if [ -n "${PID}" ]
--- stop.sh
+++ stop.sh
@@ -4,9 +4,10 @@ WORKSPACE=$1
 NAME_PROJECT=$2
 DIRECTORY=$3
 FILENAME=$4
+VALUE_PROFILE=$5
 FILEPATH=${DIRECTORY}${FILENAME}
-PROFILE=$5
-OPTION="-Dspring.profiles.active=${PROFILE}"
+KEY_PROFILE="spring.profiles.active"
+OPTION="-D${KEY_PROFILE}=${VALUE_PROFILE}"
 
 echo ========== Terminating ${NAME_PROJECT} process ==========
 
@@ -17,11 +18,12 @@ echo WORKSPACE : ${WORKSPACE}
 echo NAME_PROJECT : ${NAME_PROJECT}
 echo DIRECTORY : ${DIRECTORY}
 echo FILENAME : ${FILENAME}
+echo VALUE_PROFILE : ${VALUE_PROFILE}
 echo FILEPATH : ${FILEPATH}
-echo PROFILE : ${PROFILE}
+echo KEY_PROFILE : ${KEY_PROFILE}
 echo OPTION : ${OPTION}
 
-PID=`ps -ef | grep ${FILEPATH} | grep ${OPTION} | grep -v grep | awk '{print $2}'`
+PID=`ps -ef | grep ${FILEPATH} | grep ${KEY_PROFILE}=${VALUE_PROFILE} | grep -v grep | awk '{print $2}'`
 echo PID : ${PID}
 
 if [ -n "${PID}" ]
