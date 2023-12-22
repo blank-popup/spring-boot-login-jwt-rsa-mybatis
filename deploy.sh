b0ce84a6e9f3228c2852660fbf885094143c89a3
blank_popup <ha6446@gmail.com>
2023-12-22 22:59:50 +0900

    Test Jenkins WorkSpace

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+NAME_PROJECT="template"
 DIRECTORY="/home/JENKINS/template/api/"
 FILENAME="loginJwtRSA-0.0.1-SNAPSHOT.jar"
 PROFILE="develop"
@@ -12,31 +13,36 @@ echo DIRECTORY : ${DIRECTORY}
 echo FILENAME : ${FILENAME}
 echo PROFILE : ${PROFILE}
 
-echo Terminate template process
+echo Terminating ${NAME_PROJECT} process
 
 PID=`ps -ef | grep ${FILENAME} | grep -v grep | awk '{print $2}'`
 echo PID : ${PID}
 
 if [ -n "${PID}" ]
 then
-    result1=$(kill -9 ${PID})
+    kill -9 ${PID}
     echo process is killed.
 else
     echo running process not found.
 fi
+echo Terminated ${NAME_PROJECT} process
 
 sleep 1
 
-echo copy template file
+echo copying ${NAME_PROJECT} file
 
 FILEPATH=${DIRECTORY}${FILENAME}
 rm -rf ${FILEPATH}
+echo removed previous ${NAME_PROJECT} file
 cp -r ${WORKSPACE}/build/libs/${FILENAME} ${FILEPATH}
+echo copied new ${NAME_PROJECT} file
+echo copied ${NAME_PROJECT} file
 
 sleep 1
 
-echo Start template process
+echo Starting ${NAME_PROJECT} process
 
 OPT="-Dspring.profiles.active=${PROFILE}"
-
+echo java -jar ${OPT} "${FILEPATH}" "&"
 java -jar ${OPT} "${FILEPATH}" &
+echo Started ${NAME_PROJECT} process
