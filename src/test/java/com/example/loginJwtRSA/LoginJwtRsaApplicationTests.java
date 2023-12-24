9fc733a9b4af8cb68d86f797aa2f60b05ca29e92
blank_popup <ha6446@gmail.com>
2023-12-25 00:14:45 +0900

    gradle test failed

--- src/test/java/com/example/loginJwtRSA/LoginJwtRsaApplicationTests.java
+++ src/test/java/com/example/loginJwtRSA/LoginJwtRsaApplicationTests.java
@@ -3,9 +3,11 @@ package com.example.loginJwtRSA;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test_server")
+@TestPropertySource(properties="logging.config=classpath:logback-spring.xml")
 class LoginJwtRsaApplicationTests {
 
     @Test
